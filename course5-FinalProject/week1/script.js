40027730213b2931c334a021e962722bf45dca9a
Ivan Sharonov <48245524+isharonov@users.noreply.github.com>
2019-07-09 15:00:01 +0300

    Неделя 2: До загрузки на проверку

--- course5-FinalProject/week1/script.js
+++ course5-FinalProject/week1/script.js
@@ -1,5 +1,15 @@
 var memoji = ['🐮', '🐓', '🦃', '🐟', '🦄', '🐞'];
 
+Array.prototype.valueShuffle = function(){
+    var i = this.length, j, temp;
+    while(--i > 0){
+        j = Math.floor(Math.random() * (i+1));
+        temp = this[j];
+        this[j] = this[i];
+        this[i] = temp;
+    }
+}
+
 //Конструктор объекта карточки
 function Card(id, content) {
     this.value = content;
@@ -18,8 +28,12 @@ function Game() {
     this.playground = document.getElementById('playground');
     this.values = memoji.concat(memoji);
     this.cards = {};
+    this.cardsSelected = { length: 0 };
 }
 Game.prototype.start = function() {
+    
+    //Перемешиваем карточки
+    this.values.valueShuffle();
 
     //Раскладываем карточки
     for (var i = 0; i < this.values.length; i++) {
@@ -38,14 +52,57 @@ Game.prototype.start = function() {
 
             var target = self.cards[event.target.parentNode.id];
 
+            //0 - Игнорируем клики на уже открытые карточки
+            if (target.node.classList.contains('rotate')) {
+                return;
+            }
+
             //1 - Клик на закрытую карточку + нет открытых несовпавших карточек
-            if (!target.node.classList.contains('rotate')) {
+            if (!target.node.classList.contains('rotate') && self.cardsSelected.length < 2) {
                 
                 target.node.classList.add('rotate');
 
-            } else {
+                //1.1 - Если нет пары для сравнения
+                if (self.cardsSelected.length == 0) {
+
+                    self.cardsSelected[0] = target;
+                    self.cardsSelected.length ++ ;
+                 
+                //1.2 - Если есть пара для сравнения
+                } else if (self.cardsSelected.length == 1) {
+
+                    self.cardsSelected[1] = target;
+                    self.cardsSelected.length ++ ;
+                    
+                    //Проверяем 2 открытые карточки
+                    if (self.cardsSelected[0].value == self.cardsSelected[1].value){
+
+                        self.cardsSelected[0].node.classList.add('match');
+                        self.cardsSelected[1].node.classList.add('match');
+
+                        self.cardsSelected = { length: 0 };
+
+                    } else {
+
+                        self.cardsSelected[0].node.classList.add('mismatch');
+                        self.cardsSelected[1].node.classList.add('mismatch');
+                    }
+                }
+            //2 - Клик на закрытую карточку + есть 2 открытые несовпавшие карточки
+            } else if (!target.node.classList.contains('rotate') && self.cardsSelected.length == 2) {
             
-                target.node.classList.remove('rotate');
+                self.cardsSelected[0].node.classList.remove('rotate');
+                self.cardsSelected[1].node.classList.remove('rotate');
+
+                self.cardsSelected[0].node.classList.remove('mismatch');
+                self.cardsSelected[1].node.classList.remove('mismatch');
+
+                self.cardsSelected = { length: 0 };
+
+                target.node.classList.add('rotate');
+
+                self.cardsSelected[0] = target;
+                self.cardsSelected.length ++ ;
                 
             }
         }
