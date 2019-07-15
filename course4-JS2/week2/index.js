febc6e05b96fb6e2a2716fffe34d0a93c71dafe2
Ivan Sharonov <48245524+isharonov@users.noreply.github.com>
2019-07-15 22:27:11 +0300

    Проверка пройдена. Сделал рефакторинг на основе образца решения

--- course4-JS2/week2/index.js
+++ course4-JS2/week2/index.js
@@ -4,85 +4,60 @@ module.exports = Collection;
  * Конструктор коллекции
  * @constructor
  */
-function Collection(values) {
-    if (values) {
-        this.value = [].concat(values);
-    } else {
-        this.value = [];
-    }  
+function Collection() {
+    this._values = [];  
 }
 
 
 // Методы коллекции
 Collection.prototype.values = function () {
-    return this.value;
+    return this._values;
 };
 
 Collection.prototype.count = function() {
-    return this.value.length;
+    return this._values.length;
 }
 
 Collection.prototype.at = function(position) {
-    return this.value[position - 1] ? this.value[position - 1] : null;
+    return this._validPosition(position) 
+        ? this._values[position - 1] 
+        : null;
 };
 
-Collection.prototype.append = function(elements) {
-    if (elements instanceof Collection) {
-        for (var i = 0; i < elements.value.length; i++) {
-            this.value.push(elements.value[i]);
-        }
+Collection.prototype.removeAt = function(position) {
+    var valid = this._validPosition(position);
+
+    if (valid) {
+        this._values.splice(position - 1, 1);
+    } 
+
+    return valid;
+};
+
+Collection.prototype.append = function(item) {
+    if (item instanceof Collection) {
+        this._values = this._values.concat(item.values());
     } else {
-        this.value.push(elements);
+        this._values.push(item);
     }
     
 };
 
-Collection.prototype.removeAt = function(position) {
-    if (this.value[position - 1]) {
-        this.value.splice(position - 1, 1);
-        return true;
-    } else {
-        return false;
-    }
-};
+//Вспомогательный метод, проверяет существование передаваемой позициии в коллекции
+Collection.prototype._validPosition = function(position) {
+    return position > 0 && position <= this.count();
+}
 
 /**
  * Создание коллекции из массива значений
  */
-Collection.from = function (values) {
-   return new Collection(values);
+Collection.from = function (items) {
+   return items.reduce(
+       function(collection, item) {
+       collection.append(item);
+
+       return collection;
+        }, 
+        new Collection()
+   );
 };
-/* 
-//Testing:
-// Создаем коллекцию чисел
-var numbers = new Collection();
-numbers.append(10);
-numbers.append(20);
-console.log(numbers.count()); //2
-console.log(numbers.values()); // [10,20]
-
-// Создаем коллекцию букв
-var letters = Collection.from(['a', 'b', 'c']);
-letters.append('d');
-console.log(letters.count()); //4
-console.log(letters.values()); // ['a', 'b', 'c', 'd']
-
-// Смешиваем обе коллекции
-var items = new Collection();
-items.append(numbers);
-items.append(letters);
-console.log(items.count()); //6
-console.log(items.values()); // [10, 20, 'a', 'b', 'c', 'd']
-
-// Проверяем получение элемента
-console.log(items.at(0)) //null
-console.log(items.at(1)) //10)
-console.log(items.at(3)) //'a'
-console.log(items.at(6)) //'d'
-
-// Проверяем удаление
-console.log(items.removeAt(0)); //false
-console.log(items.removeAt(2)); //true
-console.log(items.removeAt(5)); //true
-
-console.log(items.values()) //[10, 'a', 'b', 'c'] */
