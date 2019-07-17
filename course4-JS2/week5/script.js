804bd74112ee676ac29646f6d737f5e3f3b4cf2c
Ivan Sharonov <48245524+isharonov@users.noreply.github.com>
2019-07-17 18:40:13 +0300

    Проверка пройдена. Сделал рефакторинг на основе образца решения

--- course4-JS2/week5/script.js
+++ course4-JS2/week5/script.js
@@ -1,84 +1,101 @@
 'use strict';
+//Скрываем от window все методы, кроме validateForm
+(function () {
+    function validateNumber(value, min, max) {
+        value = parseInt(value);
 
-// Код валидации формы
-function validateForm(params) {
+        if (isNaN(value)) {
+            return false;
+        }
+        // typeof(min) == string, поэтому условие выполнится даже если min == '0'
+        if (min && value < parseInt(min)) {
+            return false;
+        }
+        if (max && value > parseInt(max)) {
+            return false;
+        }
 
-    function addErrorClass(element) {
-        element.classList.add(params.inputErrorClass);
+        return true;
     }
 
-    function checkInputs(element) {
-        //Обязательность поля
-        if (element.dataset.required == "") {
-            if (!element.value) {
-                addErrorClass(element);
-            };
+    function validateRegExp(value, pattern, flags) {
+        var regExp = new RegExp(pattern, flags);
+
+        return regExp.test(value);
+    }
+
+    function validateValue(value, dataset) {
+        switch(dataset.validator) {
+            case 'letters': 
+                return validateRegExp(value, '^[a-zа-яё]+$', 'i');
+            case 'number': 
+                return validateNumber(value, dataset.validatorMin, dataset.validatorMax);
+            case 'regexp': 
+                return validateRegExp(value, dataset.validatorPattern);
+            default: 
+                return true;
         };
+    }
+
+    function checkInput(element) {
+        var value = element.value;
+        //Обязательность поля
+        if (element.dataset.hasOwnProperty('required') && !value) {
+            return false;
+        }
 
         //Валидация
-        switch(element.dataset.validator) {
-            case 'letters': //Валидатор letters
-                if (!/^[a-zA-Zа-яА-яё]*$/.test(element.value)) {
-                    addErrorClass(element);
-                }; 
-                break;
-            case 'number': //Валидатор number
-                if (!/^-?[0-9]*$/.test(element.value)) {
-                    addErrorClass(element);
-                };
-                if (parseInt(element.value) > parseInt(element.dataset.validatorMax) || 
-                    parseInt(element.value) < parseInt(element.dataset.validatorMin)) {
-                        addErrorClass(element);
-                };
-                break;
-            case 'regexp': //Валидатор regexp
-                if (element.value) {
-                    if (!element.value.match(element.dataset.validatorPattern)) {
-                        addErrorClass(element);
-                    };
-                }     
-                break;
-        };
+        var validator = element.dataset.validator;
+
+        return (validator && value) 
+            ? validateValue(value, element.dataset)
+            : true;
+        
     };
+    // Код валидации формы
+    window.validateForm = function(options) {
 
-    var form = document.getElementById(params.formId);
+        var form = document.getElementById(options.formId),
+            inputs = Array.from(form.querySelectorAll('input'));
 
-    form.addEventListener('submit', function(event){
-        event.preventDefault();
+        //Класс с ошибкой (inputErrorClass) удаляется при фокусе на элемент (focus)
+        form.addEventListener('focus', function(event) {
+            var target = event.target;
+            if (target.tagName === 'INPUT') {
+                target.classList.remove(options.inputErrorClass);
+            };
+        }, true);
 
-        //Проверка всех элементов формы
-        var elements = Array.from(form.querySelectorAll('input')),
-            errors = 0;
-        elements.forEach(function(currentElement){
-            checkInputs(currentElement);
-            if (currentElement.classList.contains(params.inputErrorClass)) {
-                errors++;
+        //При потере фокуса (blur) элемента input вызывается проверка для этого элемента
+        form.addEventListener('blur', function(event){
+            var target = event.target;
+            if (target.tagName === 'INPUT') {
+                if (!checkInput(target)) {
+                    target.classList.add(options.inputErrorClass);
+                };
+            };   
+        }, true);
+
+        form.addEventListener('submit', function(event){
+            event.preventDefault();
+            form.classList.remove(options.formValidClass);
+            form.classList.remove(options.formInvalidClass);
+
+            //Проверка всех элементов формы
+            var hasError = false;
+            inputs.forEach(function(currentInput){
+                if (!checkInput(currentInput)) {
+                    currentInput.classList.add(options.inputErrorClass);
+                    hasError = true;
+                };
+            });
+
+            //Сообщение о наличии ошибок
+            if (hasError) {
+                form.classList.add(options.formInvalidClass);
+            } else {
+                form.classList.add(options.formValidClass);
             }
         });
-
-        //Сообщение о наличии ошибок
-        if (errors) {
-            form.classList.remove(params.formValidClass);
-            form.classList.add(params.formInvalidClass);
-        } else {
-            form.classList.remove(params.formInvalidClass);
-            form.classList.add(params.formValidClass);
-        }
-    });
-
-    //Класс с ошибкой (inputErrorClass) удаляется при фокусе на элемент (focus)
-    form.addEventListener('focus', function(event) {
-        if (event.target.tagName === 'INPUT') {
-            event.target.classList.remove(params.inputErrorClass);
-        };
-    }, true);
-
-    //При потере фокуса (blur) элемента input вызывается проверка для этого элемента
-    form.addEventListener('blur', function(event){
-        if (event.target.tagName === 'INPUT') {
-            checkInputs(event.target);
-        };   
-    }, true);
-
-    
-};
+    };
+}());
