e8a193e0f55d6514dde6b9bba068d6c1e57f4c23
Ivan Sharonov <48245524+isharonov@users.noreply.github.com>
2019-07-18 18:51:50 +0300

    До загрузки на проверку

--- course4-JS2/week3/index.js
+++ course4-JS2/week3/index.js
@@ -3,5 +3,24 @@
  * @param {Function} callback
  */
 module.exports = function (operations, callback) {
+    function executeOperation(operation) {
+        return new Promise(function(resolve, reject){
+            operation(function(err, res){
+                err ? reject(err) : resolve(res);
+            });
+        });
+    }
 
+    var promises = [];
+    operations.forEach(function(item){
+        promises.push(executeOperation(item));
+    });
+
+    Promise
+        .all(promises)
+        .then(function(data){
+            callback(null, data);
+        }, function(error){
+            callback(error);
+        });
 };
