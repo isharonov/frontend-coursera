0e72ab5051639f2d52632178bb7aa966282ffb6f
Ivan Sharonov <48245524+isharonov@users.noreply.github.com>
2019-07-18 19:17:44 +0300

    Проверка пройдена. Сделал рефакторинг на основе образца решения

--- course4-JS2/week3/index.js
+++ course4-JS2/week3/index.js
@@ -3,6 +3,44 @@
  * @param {Function} callback
  */
 module.exports = function (operations, callback) {
+  
+    //Решение без использования промисов
+    //Случай, когда операции не переданы
+    if (operations.length === 0) {
+        callback(null, []);
+    }
+
+    var result = [];
+    var hasError = false;
+    var doneOperations = 0;
+
+    operations.forEach(function (operation, index) {
+        operation(function next(err, res){
+            //Если была ошибка в предыдущих операциях, не обрабатываем результат и не вызываем callback
+            if (hasError) {
+                return;
+            }
+
+            //Обрабатываем ошибки
+            if (err) {
+                callback(err);
+                hasError = true;
+
+                return;
+            }
+
+            //Сохраняем результат
+            result[index] = res;
+            doneOperations++;
+
+            //Вызываем результирующий callback
+            if (doneOperations === operations.length) {
+                callback(null, result);
+            }
+        });
+    });
+      
+  /*   //Решение с использованием промисов
     function executeOperation(operation) {
         return new Promise(function(resolve, reject){
             operation(function(err, res){
@@ -22,5 +60,6 @@ module.exports = function (operations, callback) {
             callback(null, data);
         }, function(error){
             callback(error);
-        });
+        }); 
+    */
 };
